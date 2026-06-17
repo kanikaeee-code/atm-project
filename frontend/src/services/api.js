2c9384c51308a2aa1bc06a321eb31ff6f9c8d4c3
Kanika <kanikagopalakrishnamoorthy@gmail.com>
2026-06-17 14:10:16 +0530

    Added production API URL and CORS

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Base API URL (CHANGE THIS after deployment)
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,  
+  baseURL: "https://atm-project-pfzh.onrender.com/api", 
 });
 
 // 🔐 Attach token automatically (IMPORTANT)
